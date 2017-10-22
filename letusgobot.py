c2d0e0f67db2e08b3105dc6cd5f990b2ae66307f
NataliaLev <itekimasu@mail.ru>
2017-10-22 12:34:28 +0300

    commit

--- letusgobot.py
+++ letusgobot.py
@@ -1,4 +1,5 @@
 import requests
+from requests import *
 
 url = "https://api.telegram.org/bot424475122:AAHZ8iO6KHSTmACSBVlpDH4YKHLwxuXPslc/"
 
@@ -55,7 +56,7 @@ send_mess(chat_id, 'Your message goes here')
         #return last_update
     
     #greet_bot = BotHandler(token)  
-    #greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
+    #greetings = ('Hello!', 'Hi!')  
     #now = datetime.datetime.now()
        
     #def main():  
@@ -74,15 +75,15 @@ send_mess(chat_id, 'Your message goes here')
             #last_chat_name = last_update['message']['chat']['first_name']
     
             #if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-                #greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+                #greet_bot.send_message(last_chat_id, 'Hello, {}'.format(last_chat_name))
                 #today += 1
     
             #elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-                #greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+                #greet_bot.send_message(last_chat_id, 'Hi, {}'.format(last_chat_name))
                 #today += 1
     
             #elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-                #greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+                #greet_bot.send_message(last_chat_id, 'Yay, {}'.format(last_chat_name))
                 #today += 1
     
             #new_offset = last_update_id + 1
